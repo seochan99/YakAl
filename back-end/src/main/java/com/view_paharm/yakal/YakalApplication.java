30ed81ea5d2edc45b03dce498a0c83e60975739d
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-07-04 21:33:58 +0900

    feat: 초기 구성

---
+++ back-end/src/main/java/com/view_paharm/yakal/YakalApplication.java
@@ -0,0 +1,13 @@
+package com.view_paharm.yakal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakalApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/view_paharm/yakal/YakalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.view_paharm.yakal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
