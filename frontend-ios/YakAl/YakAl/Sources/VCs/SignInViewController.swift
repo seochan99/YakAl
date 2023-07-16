8c2df0aedc6668014e92baf714636d5b9c29cee3
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-07-16 22:17:57 +0900

    feat : input TEXT 활성화 여부

--- frontend-ios/YakAl/YakAl/Sources/VCs/SignInViewController.swift
+++ frontend-ios/YakAl/YakAl/Sources/VCs/SignInViewController.swift
@@ -37,7 +37,6 @@ class SignInViewController: UIViewController {
     
     // 개인정보 동의 여부 확인
     private func setAgreeButtonState(_ button: UIButton, isOn: Bool) {
-        
         if isOn {
             button.isEnabled = true
             button.backgroundColor = UIColor(red: 38/255, green: 102/255, blue: 246/255, alpha: 1.0)
@@ -69,72 +68,78 @@ class Step1VC: SignInViewController {
     
     @IBOutlet weak var siginInInputField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
+    
     var bottomConstraint: NSLayoutConstraint?
     
     override func viewDidLoad() {
-          super.viewDidLoad()
-          myStepNumber = 1
-          
-          // Set bottom constraint for button
-          let safeArea = self.view.safeAreaLayoutGuide
-          bottomConstraint = nextButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0)
-          bottomConstraint?.isActive = true
-          
-          // Register for keyboard notifications
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-          
-          // Add target for text field's editing changed event
-          siginInInputField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
-        
-        
-        // Set initial state of the nextButton
-        updateNextButtonState()
-      }
-      
-      @IBAction func nextButtonTapped(_ sender: UIButton) {
-          let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
-          let signInScreen4VC = storyboard.instantiateViewController(withIdentifier: "SignInScreen_4") as! Step2VC
-          navigationController?.pushViewController(signInScreen4VC, animated: true)
-      }
-      
+           super.viewDidLoad()
+           myStepNumber = 1
+           
+           // Set bottom constraint for button
+           let safeArea = self.view.safeAreaLayoutGuide
+           bottomConstraint = nextButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0)
+           bottomConstraint?.isActive = true
+           
+           // Register for keyboard notifications
+           NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+           NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+           
+           // Add target for text field's editing changed event
+           siginInInputField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+           
+           // Set initial state of the nextButton
+           updateNextButtonState()
+       }
+       
+       
+       @IBAction func nextButtonTapped(_ sender: UIButton) {
+           let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
+           let signInScreen4VC = storyboard.instantiateViewController(withIdentifier: "SignInScreen_4") as! Step2VC
+           navigationController?.pushViewController(signInScreen4VC, animated: true)
+       }
+       
     // Handle text field editing changed event
-     @objc private func textFieldDidChange(_ textField: UITextField) {
-         updateNextButtonState()
-     }
-    
-    private func updateNextButtonState() {
-        if let text = siginInInputField.text, !text.isEmpty {
-            nextButton.isEnabled = true
-            nextButton.backgroundColor = UIColor(red: 38/255, green: 102/255, blue: 246/255, alpha: 1.0)
-            nextButton.setTitleColor(.white, for: .normal)
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        if textField.text?.isEmpty == false {
+            siginInInputField.layer.borderWidth = 2.0
+            siginInInputField.layer.cornerRadius = 8
+            siginInInputField.layer.borderColor = UIColor(red: 85/255, green: 136/255, blue: 253/255, alpha: 1.0).cgColor
         } else {
-            nextButton.isEnabled = false
-            nextButton.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 238/255, alpha: 1.0)
-            nextButton.setTitleColor(UIColor(red: 198/255, green: 198/255, blue: 207/255, alpha: 1.0), for: .disabled)
+//            siginInInputField.layer.borderWidth = 2.0
+//            siginInInputField.layer.cornerRadius = 8
+//            siginInInputField.layer.borderColor = UIColor(red: 233/255, green: 233/255, blue: 238/255, alpha: 1.0).cgColor
         }
+        updateNextButtonState()
     }
-      
-      // Show the keyboard
-      @objc private func keyboardWillShow(_ notification: NSNotification) {
-          if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-              let keyboardHeight = keyboardSize.height - self.view.safeAreaInsets.bottom
-              bottomConstraint?.constant = -(keyboardHeight + 15)
-              self.view.layoutIfNeeded()
-          }
-      }
-      
-      // Hide the keyboard
-      @objc private func keyboardWillHide(_ notification: NSNotification) {
-          bottomConstraint?.constant = 0
-          self.view.layoutIfNeeded()
-      }
+       
+       // Update the state of the nextButton based on the text field's content
+       private func updateNextButtonState() {
+           if let text = siginInInputField.text, !text.isEmpty {
+               nextButton.isEnabled = true
+               nextButton.backgroundColor = UIColor(red: 38/255, green: 102/255, blue: 246/255, alpha: 1.0)
+               nextButton.setTitleColor(.white, for: .normal)
+           } else {
+               nextButton.isEnabled = false
+               nextButton.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 238/255, alpha: 1.0)
+               nextButton.setTitleColor(UIColor(red: 198/255, green: 198/255, blue: 207/255, alpha: 1.0), for: .disabled)
+           }
+       }
+       
+       // Show the keyboard
+       @objc private func keyboardWillShow(_ notification: NSNotification) {
+           if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+               let keyboardHeight = keyboardSize.height - self.view.safeAreaInsets.bottom
+               bottomConstraint?.constant = -(keyboardHeight + 15)
+               self.view.layoutIfNeeded()
+           }
+       }
+       
+       // Hide the keyboard
+       @objc private func keyboardWillHide(_ notification: NSNotification) {
+           bottomConstraint?.constant = 0
+           self.view.layoutIfNeeded()
+       }
 }
-
-
-
-
-
 //MARK: - 생년월일
 class Step2VC: SignInViewController {
     @IBOutlet weak var birthPicker: UIDatePicker!
@@ -161,6 +166,7 @@ class Step3VC: SignInViewController
     @IBOutlet weak var tfInput: UITextField!
     @IBOutlet weak var ivIcon: UIImageView!
     @IBOutlet weak var btnSelect: UIButton!
+    @IBOutlet weak var siginInInputField: UITextField!
     
     var bottomConstraint: NSLayoutConstraint?
     
@@ -171,8 +177,6 @@ class Step3VC: SignInViewController
     let itemList = ["SKT", "KT", "LG U+", "SKT 알뜰폰", "KT 알뜰폰", "LG U+ 알뜰폰"]
     
     override func viewDidLoad() {
-        
-        
         super.viewDidLoad()
         myStepNumber = 3
         
@@ -180,6 +184,23 @@ class Step3VC: SignInViewController
         initUI();
         setDropdown();
         // maybe some other stuff specific to this "step"
+        
+        
+        // Set bottom constraint for button
+        let safeArea = self.view.safeAreaLayoutGuide
+        bottomConstraint = nextButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0)
+        bottomConstraint?.isActive = true
+        
+        // Register for keyboard notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // Add target for text field's editing changed event
+        siginInInputField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+        // Set initial state of the nextButton
+        updateNextButtonState()
+        
     }
     
     
@@ -204,7 +225,6 @@ class Step3VC: SignInViewController
         DropDown.appearance().selectionBackgroundColor = UIColor.lightGray // 선택한 아이템 배경 색상
         DropDown.appearance().setupCornerRadius(8)
         dropdown.dismissMode = .automatic // 팝업을 닫을 모드 설정
-        
         tfInput.isUserInteractionEnabled = false
         ivIcon.tintColor = UIColor.gray
         
@@ -245,16 +265,76 @@ class Step3VC: SignInViewController
             ivIcon.image = UIImage(systemName: "chevron.up")
         }
     }
-
+    
+    // Handle text field editing changed event
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        if textField.text?.isEmpty == false {
+            siginInInputField.layer.borderWidth = 2.0
+            siginInInputField.layer.cornerRadius = 8
+            siginInInputField.layer.borderColor = UIColor(red: 85/255, green: 136/255, blue: 253/255, alpha: 1.0).cgColor
+        } else {
+//            siginInInputField.layer.borderWidth = 2.0
+//            siginInInputField.layer.cornerRadius = 8
+//            siginInInputField.layer.borderColor = UIColor(red: 233/255, green: 233/255, blue: 238/255, alpha: 1.0).cgColor
+        }
+        updateNextButtonState()
+    }
+       
+       // Update the state of the nextButton based on the text field's content
+       private func updateNextButtonState() {
+           if let text = siginInInputField.text, !text.isEmpty,  let inputText = tfInput.text, !inputText.isEmpty {
+               nextButton.isEnabled = true
+               nextButton.backgroundColor = UIColor(red: 38/255, green: 102/255, blue: 246/255, alpha: 1.0)
+               nextButton.setTitleColor(.white, for: .normal)
+           } else {
+               nextButton.isEnabled = false
+               nextButton.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 238/255, alpha: 1.0)
+               nextButton.setTitleColor(UIColor(red: 198/255, green: 198/255, blue: 207/255, alpha: 1.0), for: .disabled)
+           }
+       }
+       
+       // Show the keyboard
+       @objc private func keyboardWillShow(_ notification: NSNotification) {
+           if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+               let keyboardHeight = keyboardSize.height - self.view.safeAreaInsets.bottom
+               bottomConstraint?.constant = -(keyboardHeight + 15)
+               self.view.layoutIfNeeded()
+           }
+       }
+       
+       // Hide the keyboard
+       @objc private func keyboardWillHide(_ notification: NSNotification) {
+           bottomConstraint?.constant = 0
+           self.view.layoutIfNeeded()
+       }
+    
 }
 //MARK: - 문자인증
 class Step4VC: SignInViewController {
     
+    @IBOutlet weak var siginInInputField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    var bottomConstraint: NSLayoutConstraint?
     override func viewDidLoad() {
         super.viewDidLoad()
         myStepNumber = 4
         
         // maybe some other stuff specific to this "step"
+        // Set bottom constraint for button
+        let safeArea = self.view.safeAreaLayoutGuide
+        bottomConstraint = nextButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0)
+        bottomConstraint?.isActive = true
+        
+        // Register for keyboard notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // Add target for text field's editing changed event
+        siginInInputField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+        // Set initial state of the nextButton
+        updateNextButtonState()
     }
     
     @IBAction func goMainButton(_ sender: UIButton) {
@@ -263,5 +343,50 @@ class Step4VC: SignInViewController {
            navigationController?.setNavigationBarHidden(true, animated: false)
            navigationController?.pushViewController(homeScreen1VC, animated: true)
     }
+    
+    
+    
+    
+    // Handle text field editing changed event
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        if textField.text?.isEmpty == false {
+            siginInInputField.layer.borderWidth = 2.0
+            siginInInputField.layer.cornerRadius = 8
+            siginInInputField.layer.borderColor = UIColor(red: 85/255, green: 136/255, blue: 253/255, alpha: 1.0).cgColor
+        } else {
+//            siginInInputField.layer.borderWidth = 2.0
+//            siginInInputField.layer.cornerRadius = 8
+//            siginInInputField.layer.borderColor = UIColor(red: 233/255, green: 233/255, blue: 238/255, alpha: 1.0).cgColor
+        }
+        updateNextButtonState()
+    }
+       
+       // Update the state of the nextButton based on the text field's content
+       private func updateNextButtonState() {
+           if let text = siginInInputField.text, !text.isEmpty {
+               nextButton.isEnabled = true
+               nextButton.backgroundColor = UIColor(red: 38/255, green: 102/255, blue: 246/255, alpha: 1.0)
+               nextButton.setTitleColor(.white, for: .normal)
+           } else {
+               nextButton.isEnabled = false
+               nextButton.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 238/255, alpha: 1.0)
+               nextButton.setTitleColor(UIColor(red: 198/255, green: 198/255, blue: 207/255, alpha: 1.0), for: .disabled)
+           }
+       }
+       
+       // Show the keyboard
+       @objc private func keyboardWillShow(_ notification: NSNotification) {
+           if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+               let keyboardHeight = keyboardSize.height - self.view.safeAreaInsets.bottom
+               bottomConstraint?.constant = -(keyboardHeight + 15)
+               self.view.layoutIfNeeded()
+           }
+       }
+       
+       // Hide the keyboard
+       @objc private func keyboardWillHide(_ notification: NSNotification) {
+           bottomConstraint?.constant = 0
+           self.view.layoutIfNeeded()
+       }
 }
 
