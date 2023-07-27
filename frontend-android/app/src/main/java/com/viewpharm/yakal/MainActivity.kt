fc1a5feddcaa588c96e909cc7030f11affe2d06d
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-07-27 12:00:16 +0900

    feat: Android 초기화

---
+++ frontend-android/app/src/main/java/com/viewpharm/yakal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.viewpharm.yakal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
