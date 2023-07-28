961af6b88d05ee203e0ac09aac74ffc83585d16c
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-07-28 22:14:21 +0900

    feat: Splash 화면 연결 및 아이콘 변경

--- frontend-android/app/src/main/java/com/viewpharm/yakal/MainActivity.kt
+++ frontend-android/app/src/main/java/com/viewpharm/yakal/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.viewpharm.yakal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
