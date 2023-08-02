e51c3cc3341b57b2b7d15761e044939b261ea47c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-02 12:34:59 +0900

    feat : 약 추가 버튼 클릭시 이미지 및 모달창 생성

--- frontend-ios/YakAl/YakAl/Sources/VCs/Home/HomeVC.swift
+++ frontend-ios/YakAl/YakAl/Sources/VCs/Home/HomeVC.swift
@@ -11,14 +11,41 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var calendarView: UIView!
     
-    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var floatingStackView: UIStackView!
+    
     @IBOutlet weak var myTodoTableView: UITableView!
     
     @IBOutlet weak var emptyView: UIView!
     
     @IBOutlet weak var addModalView: UIView!
+    @IBOutlet weak var AddModalButtonView: UIView!
     
+//    버튼
+    @IBOutlet weak var EnvelopeMedicineButton: UIButton!
+    @IBOutlet weak var normalMedicineButton: UIButton!
+    @IBOutlet weak var DirectMedicineButton: UIButton!
+    
+    @IBOutlet weak var addMedicineButton: UIButton!
+    
+    
+    // 버튼들이 들어있는 모달 뷰
+    lazy var buttons: [UIView] = [self.addModalView]
 
+    // 플로팅 버튼 상태에 대한 플래그
+    var isShowFloating: Bool = false
+
+
+    // 배경 어둡게 만드는 view
+    lazy var floatingDimView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+        
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        view.alpha = 0
+        view.isHidden = true
+
+        return view
+    }()
+    
     
     // 이곳에 해당하는 데이터 배열이 있을 것으로 가정합니다.
      var myTodoItems: [TodoItem] = [] // TodoItem은 Todo 아이템 데이터 모델로 가정합니다.
@@ -43,15 +70,18 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // UIColor 객체를 #E9E9EE 색상으로 생성
     let borderColor = UIColor(red: 233.0/255.0, green: 233.0/255.0, blue: 238.0/255.0, alpha: 1.0)
+    
+
+    
+    //MARK: - viewDidLoad
     override func viewDidLoad() {
-
-
+        super.viewDidLoad()
+        
         // calendarView의 레이어(border)의 색상을 설정
         calendarView.layer.borderColor = borderColor.cgColor
         addModalView.layer.borderColor = borderColor.cgColor
-        super.viewDidLoad()
+      
     }
-    
     
     // 아래 메서드를 사용하여 emptyView의 상태를 갱신합니다.
     func updateEmptyViewVisibility() {
@@ -61,5 +91,64 @@ class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
             emptyView.isHidden = true
         }
     }
+    
+    
+  
+    @IBAction func EnvelopeMedicineButtonAction(_ sender: UIButton) {
+        print("EnvelopeMedicineButtonAction")
+    }
+    @IBAction func normalMedicineButtonAction(_ sender: UIButton) {
+        print("normalMedicineButtonAction")
+    }
+    @IBAction func DirectMedicineButtonAction(_ sender: UIButton) {
+        print("DirectMedicineButtonAction")
+    }
+    
+    
+    @IBAction func floatingButtonAction(_ sender: UIButton) {
+        print("약 추가 버튼 선택됨")
+         if isShowFloating {
+             buttons.reversed().forEach { button in
+                 UIView.animate(withDuration: 0.1) {
+                     button.isHidden = true
+                     self.view.layoutIfNeeded()
+                 }
+             }
+
+             UIView.animate(withDuration: 0.1, animations: {
+                 self.floatingDimView.alpha = 0.5
+             }) { (_) in
+                 self.floatingDimView.isHidden = true
+             }
+         } else {
+
+             self.floatingDimView.isHidden = false
+
+             UIView.animate(withDuration: 0.5) {
+                 self.floatingDimView.alpha = 1
+             }
+
+             buttons.forEach { [weak self] button in
+                 button.isHidden = false
+                 button.alpha = 0
+
+                 UIView.animate(withDuration: 0.3) {
+                     button.alpha = 1
+                     self?.view.layoutIfNeeded()
+                 }
+             }
+         }
+
+         isShowFloating = !isShowFloating
+
+         let image = isShowFloating ? UIImage(named: "button-add-pill") : UIImage(named: "FAB-add-pill")
+         let roatation = isShowFloating ? CGAffineTransform(rotationAngle: .pi - (.pi / 4)) : CGAffineTransform.identity
+
+         UIView.animate(withDuration: 0.3) {
+             sender.setImage(image, for: .normal)
+             sender.transform = roatation
+         }
+
+     }
 
 }
