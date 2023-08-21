e5940a55fe2cb8cb70af5792172403ab3ead6e4e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-21 14:20:35 +0900

    feat: 달력 View, 현재 월 표시하기

--- frontend-ios/YakAl/YakAl/Sources/Views/CalendarSwiftUIView.swift
+++ frontend-ios/YakAl/YakAl/Sources/Views/CalendarSwiftUIView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
-struct DayModel: Identifiable {
+//MARK: - Day Model
+struct DayModel: Identifiable,Hashable {
     let id = UUID()
     let date: Date
     let medications: [Medicine] // List of medications for the day
@@ -10,12 +11,15 @@ struct DayModel: Identifiable {
         formatter.dateFormat = "d"
         return formatter.string(from: date)
     }
-    
+
+    // 한국어 요일 변환
     var weekdayText: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "E"
+        formatter.locale = Locale(identifier: "ko_KR") // 한국어 로케일 설정
         return formatter.string(from: date)
     }
+
     var fullWeekdayText: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "EEEE"
@@ -23,35 +27,41 @@ struct DayModel: Identifiable {
     }
 }
 
+//MARK: - Calendar Week View : 매 주차 뷰
 struct CalendarWeekView: View {
-    var weekDates: [Date]
+    // 모델 생성
+    var weekDates: [DayModel]
     
+    // 뷰
     var body: some View {
         HStack(spacing: 30) {
-            VStack {
-                HStack{
-                    Text("일")
-                    Text("월")
-                    Text("화")
-                    Text("수")
-                    Text("목")
-                    Text("금")
-                    Text("토")
-                    
-                }
-                
             ForEach(weekDates, id: \.self) { date in
-                
                 VStack {
-                    Text("\(date.day)")
+                    Text("\(date.weekdayText)")
                         .font(.headline)
-                }
-                    
+                        .foregroundColor(weekdayColor(for: date.weekdayText))
+                    Text("\(date.dayText)")
+                        .font(.headline)
+                        .foregroundColor(weekdayColor(for: date.weekdayText))
                 }
             }
         }
     }
+    
+    func weekdayColor(for weekdayText: String) -> Color {
+        switch weekdayText {
+            case "일":
+                return Color(red: 0.88, green: 0.06, blue: 0.16) // 일요일 색상
+            case "토":
+                return Color(red: 0.33, green: 0.53, blue: 0.99) // 토요일 색상
+            default:
+                return Color.black // 기본 색상
+        }
+    }
+    
 }
+
+
 
 struct CalendarSwiftUIView: View {
     @State private var currentDate = Date()
@@ -62,6 +72,24 @@ struct CalendarSwiftUIView: View {
         formatter.dateFormat = "yyyy년 M월"
         return formatter
     }
+    // Sample data for medications
+    var sampleMedications: [Medicine] = [
+        Medicine(id: 1, image: "image_덱시로펜정", name: "약물A", ingredients: "성분 A", dangerStat: 0, isTaken: false),
+        Medicine(id: 2, image: "image_덱시로펜정", name: "약물B", ingredients: "성분 B", dangerStat: 1, isTaken: false)
+    ]
+    // Generate sample DayModels for a week
+      var sampleWeekDates: [DayModel] {
+          var weekDates: [DayModel] = []
+          let calendar = Calendar.current
+          let startDate = calendar.startOfDay(for: currentDate)
+          for day in 0..<7 {
+              if let date = calendar.date(byAdding: .day, value: day, to: startDate) {
+                  let dayModel = DayModel(date: date, medications: sampleMedications)
+                  weekDates.append(dayModel)
+              }
+          }
+          return weekDates
+      }
 
     var body: some View {
         ScrollView{
@@ -82,7 +110,7 @@ struct CalendarSwiftUIView: View {
                             .weight(.semibold)
                     )
                     .foregroundColor(Color(red: 0.38, green: 0.38, blue: 0.45))
-                
+
                 Button(action: {
                     self.currentDate = Calendar.current.date(byAdding: .month, value: 1, to: self.currentDate) ?? self.currentDate
                 }) {
@@ -91,32 +119,25 @@ struct CalendarSwiftUIView: View {
                 Spacer()
             }
             
+            // 펼쳐졌을 경우의 뷰
             if isExpanded {
-                ForEach(0..<5, id: \.self) { _ in
-                    // Calculate week dates and pass them to CalendarWeekView
-                    CalendarWeekView(weekDates: [Date(), Date(), Date(), Date(), Date(), Date(), Date()])
-                }
-            } else {
-                CalendarWeekView(weekDates: [Date(), Date(), Date(), Date(), Date(), Date(), Date()])
-            }
+                         ForEach(0..<5, id: \.self) { _ in
+                             // Display sample week dates using CalendarWeekView
+                             CalendarWeekView(weekDates: sampleWeekDates)
+                         }
+                     } else {
+             // 접혔을떄
+                         CalendarWeekView(weekDates: sampleWeekDates)
+                     }
             
+            // 달력 펼치기
             Button(action: {
                 self.isExpanded.toggle()
             }) {
                 Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
             }
             
-            Rectangle()
-                .foregroundColor(Color(red: 0.96, green: 0.96, blue: 0.98))
-                .frame(width: 380, height: 200) // Adjust the height as needed
-                .cornerRadius(8)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .inset(by: 0.5)
-                        .stroke(Color(red: 0.91, green: 0.91, blue: 0.93), lineWidth: 1)
-                )
-            
-            // Loop through DayModels and display medication info
+            // 약물 뷰
             ForEach(0..<5) { _ in
                 let dummyDayModel = DayModel(date: Date(), medications: [
                     Medicine(id: 1, image: "image_덱시로펜정", name: "약물A", ingredients: "성분 A", dangerStat: 0, isTaken: false),
@@ -141,16 +162,16 @@ struct CalendarSwiftUIView: View {
 }
 
 
-struct CalendarSwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        CalendarSwiftUIView()
-    }
-}
-
 extension Date {
     var day: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "d"
         return formatter.string(from: self)
+    }
+    
+}
+struct CalendarSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarSwiftUIView()
     }
 }
