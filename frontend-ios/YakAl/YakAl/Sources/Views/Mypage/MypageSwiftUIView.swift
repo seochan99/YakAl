0c63fa1e586ae4a4f12190ca585755ebe7445abb
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-22 03:51:16 +0900

    feat: 닉네임 수정창

--- frontend-ios/YakAl/YakAl/Sources/Views/Mypage/MypageSwiftUIView.swift
+++ frontend-ios/YakAl/YakAl/Sources/Views/Mypage/MypageSwiftUIView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
 
 struct MypageSwiftUIView: View {
-    @State private var user = User.shared
+    @ObservedObject private var user = User.shared
+    @State private var showEditNicknameModal: Bool = false
+    @State private var editedNickname: String = ""
 
+    
     var body: some View {
         NavigationView {
             
@@ -25,7 +28,8 @@ struct MypageSwiftUIView: View {
                         }
                         Spacer()
                         Button("수정"){
-                            
+                            showEditNicknameModal.toggle()
+
                         }.padding(.horizontal, 12)
                             .padding(.vertical, 10)
                             .background(Color(red: 0.91, green: 0.91, blue: 0.93))
@@ -35,6 +39,12 @@ struct MypageSwiftUIView: View {
 
                     }.padding(.top, 60)
                     .padding(.horizontal, 20)
+                    .sheet(isPresented: $showEditNicknameModal) {
+                        EditNicknameView(nickname: $editedNickname, onDismiss: {
+                            showEditNicknameModal = false
+                            user.nickName = editedNickname
+                        })
+                    }
                     
                     /*-------------------- 전문가 공유 버튼 --------------------*/
                     Button(action: {
@@ -141,6 +151,67 @@ struct MypageSwiftUIView: View {
             }
             
         }
+}
+
+struct EditNicknameView: View {
+    @ObservedObject private var user = User.shared
+    @Binding var nickname: String
+    var onDismiss: () -> Void
+    @State private var isEditing: Bool = false // 추가
+
+
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            HStack {
+                VStack(alignment: .leading) { // 왼쪽 정렬을 위해 VStack에 alignment 추가
+                    Text("닉네임 수정")
+                      .font(Font.custom("SUIT", size: 20).weight(.bold))
+                      .foregroundColor(Color(red: 0.08, green: 0.08, blue: 0.08))
+                    
+                    Text("약알이 어떻게 불러드릴까요?")
+                      .font(Font.custom("SUIT", size: 13).weight(.medium))
+                      .foregroundColor(Color(red: 0.38, green: 0.38, blue: 0.45))
+                }
+                
+                Spacer() // 왼쪽과 오른쪽 요소 사이에 최대 공간을 넣음
+                VStack{
+                    Button(action: onDismiss) {
+                        Image(systemName: "xmark")
+                    }
+                    .padding()
+                }
+            }.padding(.top, 40)
+            
+            /* ----------- 닉네임 입력 ----------- */
+            TextField("닉네임 입력", text: $nickname, onEditingChanged: { editing in
+                   isEditing = editing
+               })
+                .padding()
+                .background(Color.white)
+                .cornerRadius(8)
+                .overlay(
+                       RoundedRectangle(cornerRadius: 8)
+                           .inset(by: 1)
+                           .stroke(isEditing ? Color(red: 0.33, green: 0.53, blue: 0.99) : Color.gray, lineWidth: 2)
+                   )
+
+            /* ----------- 완료 버튼 ----------- */
+            Button("완료") {
+                user.nickName = nickname
+                onDismiss()
+            }.font(
+                Font.custom("SUIT", size: 20)
+                .weight(.semibold)
+                )
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)  // 수평 방향을 꽉 채움
+                .frame(height: 56)
+                .background(Color(red: 0.15, green: 0.4, blue: 0.96))
+                .cornerRadius(8)
+        }.padding(.horizontal, 20)  // 양쪽에 마진 20 추가
+        Spacer()
+    }
 }
 
 struct MypageSwiftUIView_Previews: PreviewProvider {
