a101fbd308725e2e9aba4f7daa9afbfa79d9cd66
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-08-22 21:32:59 +0900

    feat: 달력 해당월만 보이게 하기

--- frontend-ios/YakAl/YakAl/Sources/Views/CalendarSwiftUIView.swift
+++ frontend-ios/YakAl/YakAl/Sources/Views/CalendarSwiftUIView.swift
@@ -30,33 +30,62 @@ struct DayModel: Identifiable,Hashable {
 //MARK: - Calendar Week View : 매 주차 뷰
 struct CalendarWeekView: View {
     var weekDates: [DayModel]
-    
     var body: some View {
-        HStack(spacing:0) {
-            ForEach(weekDates, id: \.self) { date in
-                VStack {
-                    if isSameDate(date.date, Date()) {
-                        // date.medicineIntakeRate
-                        CircularProgressBar(progress: 0.6, dateText: date.dayText, today:true, weekDates:date.weekdayText)
+        HStack(spacing:20) {
+                ForEach(weekDates, id: \.self) { date in
+                    
+                    if isLastWeek() && date.date.day == "1" {
+                        CircularProgressBar(progress: 0, dateText: "  ", today: false, weekDates: date.weekdayText)
+                            .padding(.top, 4)
+                    }
+                    
+                    else if isFirstWeek() && date.date.day == "1"{
+                        if isLastOne(in: weekDates, current: date) {
+                            CircularProgressBar(progress: 0.9, dateText: date.dayText, today: isSameDate(date.date, Date()), weekDates: date.weekdayText)
+                                .padding(.top, 4)
+                        } else {
+                            CircularProgressBar(progress: 0, dateText: "  ", today: false, weekDates: date.weekdayText)
+                                .padding(.top, 4)
+                        }
+                    }
+                    
+                    
+                   else if isLastWeek() && Int(date.date.day)! <= 7 {
+                        // Hide dates of next month if in the last week
+                        CircularProgressBar(progress: 0, dateText: "  ", today: false, weekDates: date.weekdayText)
                             .padding(.top, 4)
                     } else {
-                        CircularProgressBar(progress: 0.2
-                                            , dateText: date.dayText, today:false, weekDates:date.weekdayText)
+                        CircularProgressBar(progress: 0.2, dateText: date.dayText, today: isSameDate(date.date, Date()), weekDates: date.weekdayText)
                             .padding(.top, 4)
-
                     }
-                        
                 }
-            }
-        }.padding(.horizontal,20)
+            }.padding(.horizontal,20)
         
-        
-    }
-    
+      }
     func isSameDate(_ date1: Date, _ date2: Date) -> Bool {
         let calendar = Calendar.current
         return calendar.isDate(date1, inSameDayAs: date2)
     }
+    
+    func isFirstWeek() -> Bool {
+        return weekDates.contains(where: { $0.date.day == "1" })
+    }
+    
+    
+    func isLastOne(in dates: [DayModel], current: DayModel) -> Bool {
+        if let lastOne = dates.last(where: { $0.date.day == "1" }) {
+            return lastOne.id == current.id
+        }
+        return false
+    }
+    
+    func isLastWeek() -> Bool {
+        print("weekDates 는 \(weekDates)")
+        return weekDates.contains(where: { $0.date.day == "28" || $0.date.day == "29" || $0.date.day == "30" || $0.date.day == "31" })
+    }
+    
+    
+    
     
     func weekdayColor(for weekdayText: String) -> Color {
         switch weekdayText {
@@ -102,33 +131,35 @@ struct CalendarSwiftUIView: View {
         var allWeekDates: [[DayModel]] = []
         
         let calendar = Calendar.current
-        
         let components = calendar.dateComponents([.year, .month], from: currentDate)
-        // 2023-07-31 15:00:00 +0000
-    
-        
         guard let startOfMonth = calendar.date(from: components),
               let range = calendar.range(of: .weekOfMonth, in: .month, for: startOfMonth) else {
             return allWeekDates
         }
         
-        print("startOfMonth : \(startOfMonth)")
-        
-        for week in range {
-            print(week)
-            var weekDates: [DayModel] = []
-            let startOfWeek = calendar.date(bySetting: .weekOfMonth, value: week, of: startOfMonth)!
-            print(startOfWeek)
-            for day in 0..<7 {
-                if let date = calendar.date(byAdding: .day, value: day, to: startOfWeek) {
-                    let dayModel = DayModel(date: date, medications: sampleMedications)
-                    weekDates.append(dayModel)
+        if let firstWeekday = calendar.dateComponents([.weekday], from: startOfMonth).weekday {
+            var currentDay = startOfMonth
+            
+            for _ in range {
+                var weekDates: [DayModel] = []
+                
+                for dayIndex in 0..<7 {
+                    if dayIndex >= firstWeekday - 1 || allWeekDates.count > 0 { // If the index has passed the first day of the month or it's not the first week anymore
+                        let dayModel = DayModel(date: currentDay, medications: sampleMedications)
+                        weekDates.append(dayModel)
+                        currentDay = calendar.date(byAdding: .day, value: 1, to: currentDay)!
+                    } else {
+                        weekDates.append(DayModel(date: Date(timeIntervalSince1970: 0), medications: [])) // Empty day
+                    }
                 }
+                allWeekDates.append(weekDates)
             }
-            allWeekDates.append(weekDates)
         }
+        
         return allWeekDates
     }
+
+
 
     
     
@@ -269,7 +300,6 @@ struct CalendarSwiftUIView: View {
 //MARK: - 원 프로그레스바 : 달력 뷰
 struct CircularProgressBarWithText: View {
     var progress: Double
-    
     var body: some View {
         ZStack {
             Circle()
@@ -301,34 +331,41 @@ struct CircularProgressBar: View {
     var weekDates : String
     var body: some View {
         ZStack {
-            Circle()
-                .stroke(lineWidth: 3)
-                .opacity(0.3)
-                .foregroundColor(Color(red: 0.76, green: 0.82, blue: 1))
-            Circle()
-                .trim(from: 0, to: CGFloat(min(self.progress, 1.0)))
-                .stroke(style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round))
-                .foregroundColor(Color(red: 0.33, green: 0.53, blue: 0.99))
-                .rotationEffect(Angle(degrees: -90))
-            VStack{
-                if(today == true)
-                {
-                    Circle()
-                        .fill(Color(red: 0.33, green: 0.53, blue: 0.99))
-                        .frame(width: 24, height: 24)
-                        .overlay(
-                            Text("\(dateText)")
-                                .font(Font.custom("SUIT", size: 12).weight(.medium))
-                                .foregroundColor(Color.white)
-                        )
-                }else{
-                    Text(dateText)
-                        .foregroundColor(weekdayColor(for: weekDates))
-                        .font(Font.custom("SUIT", size: 12).weight(.medium))
-                        .foregroundColor(Color.black)
-                }
+            if(dateText=="  ")
+            {
                 
+            }else{
+                Circle()
+                    .stroke(lineWidth: 3)
+                    .opacity(0.3)
+                    .foregroundColor(Color(red: 0.76, green: 0.82, blue: 1))
+                Circle()
+                    .trim(from: 0, to: CGFloat(min(self.progress, 1.0)))
+                    .stroke(style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(Color(red: 0.33, green: 0.53, blue: 0.99))
+                    .rotationEffect(Angle(degrees: -90))
+                VStack{
+                    if(today == true)
+                    {
+                        Circle()
+                            .fill(Color(red: 0.33, green: 0.53, blue: 0.99))
+                            .frame(width: 24, height: 24)
+                            .overlay(
+                                Text("\(dateText)")
+                                    .font(Font.custom("SUIT", size: 12).weight(.medium))
+                                    .foregroundColor(Color.white)
+                            )
+                    }else{
+
+                        Text(dateText)
+                            .foregroundColor(weekdayColor(for: weekDates))
+                            .font(Font.custom("SUIT", size: 12).weight(.medium))
+                            .foregroundColor(Color.black)
+                    }
+                    
+                }
             }
+
             
         }
         .frame(width: 32, height: 32)
