2e899c9feb94f528c31882b5249d8ebe15741458
Dohun_Kim <raaak8128@gmail.com>
2023-08-26 12:35:17 +0900

    feat: footer reactive UI

--- frontend-web-expert/vite.config.ts
+++ frontend-web-expert/vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ svgrOptions: {} }), mkcert()],
+  plugins: [react(), svgr({ svgrOptions: {} })], // , mkcert()
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
