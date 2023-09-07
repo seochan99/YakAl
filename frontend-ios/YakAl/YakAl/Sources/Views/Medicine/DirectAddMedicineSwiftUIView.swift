71fe12d8a6d4a8c3501747a7d37eec8558010e82
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-09-07 21:57:53 +0900

    feat: 검색창 약물 가져오기

--- frontend-ios/YakAl/YakAl/Sources/Views/Medicine/DirectAddMedicineSwiftUIView.swift
+++ frontend-ios/YakAl/YakAl/Sources/Views/Medicine/DirectAddMedicineSwiftUIView.swift
@@ -1,48 +1,74 @@
 import SwiftUI
 import Combine
 
+
+struct SearchMedicineModel: Identifiable {
+    let id = UUID()
+    let Name: String
+    let Code: String
+}
+
 struct DirectAddMedicineSwiftUIView: View {
     @State private var keyword: String = ""
-    @State private var medicineList: [String] = []
     @State private var isLoading: Bool = false
-    private let keywordPublisher = PassthroughSubject<Void, Never>()
+    @State private var isEditing: Bool = false
+    @State private var medicineList: [SearchMedicineModel] = []
 
     var body: some View {
-        VStack {
-            TextField("약물 검색", text: $keyword)
-                .padding()
-                .onReceive(keywordPublisher.debounce(for: 0.5, scheduler: RunLoop.main)) { _ in
-                                    searchMedicine()
-                                }
-                .foregroundColor(.clear)
-                .cornerRadius(8)
-                .frame(height: 64)
-                .overlay(
+        VStack(spacing: 10) {
+            TextField("약 이름 입력", text: $keyword, onEditingChanged: { editing in
+                self.isEditing = editing
+            })
+            .padding()
+            .foregroundColor(Color(red: 0.08, green: 0.08, blue: 0.08))
+            .cornerRadius(8)
+            .frame(height: 64)
+            .overlay(
                 RoundedRectangle(cornerRadius: 8)
                 .inset(by: 1)
-                .stroke(Color(red: 0.91, green: 0.91, blue: 0.93), lineWidth: 2)
-                )
-
-
-            Button(action: {
+                .stroke(isEditing ? Color(red: 0.33, green: 0.53, blue: 0.99) : Color(red: 0.91, green: 0.91, blue: 0.93), lineWidth: 2)
+            )
+            .onChange(of: keyword) { newValue in
                 searchMedicine()
-            }) {
-                Text("검색")
             }
-            .padding()
             
-            if isLoading {
-                ProgressView()
-            } else {
-                List(medicineList, id: \.self) { medicine in
-                    Text(medicine)
+            ScrollView(showsIndicators: false){
+                if isLoading {
+                    ProgressView()
+                } else {
+                    VStack(alignment: .leading, spacing: 30) {
+                        if medicineList.isEmpty {
+                            if !keyword.isEmpty {
+                                Text("\(keyword) 검색결과 없습니다")
+                                    .font(Font.custom("SUIT", size: 14).weight(.medium))
+                                    .foregroundColor(Color(red: 0.08, green: 0.08, blue: 0.08))
+                            }
+                        } else {
+                            ForEach(medicineList) { medicine in
+                                Text(medicine.Name)  // Access the 'name' property here
+                                    .font(Font.custom("SUIT", size: 14).weight(.medium))
+                                    .foregroundColor(Color(red: 0.08, green: 0.08, blue: 0.08))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding() // Add padding to increase the tappable area
+                                    .onTapGesture {
+                                        self.keyword = medicine.Name
+                                    }
+                            }
+                            Spacer()
+                        }
+                    }
                 }
             }
+
+
+            BlueHorizontalButton(text: "다음", action: {}, isEnabled: true)
+                .padding(.bottom,15)
         }
         .padding(.horizontal,20)
-        .padding(.top,20)
+        .padding(.top,40)
     }
     
+    //
     func searchMedicine() {
         isLoading = true
         medicineList = []
@@ -76,34 +102,32 @@ struct DirectAddMedicineSwiftUIView: View {
         request.addValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            defer { isLoading = false }
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let data = data else {
-                print("Error: No data received")
-                return
-            }
-            
-            do {
-                if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                    print("JSON Response: \(jsonResponse)")
-                    
-                    if let list = jsonResponse["List"] as? [[String: Any]] {
-                        self.medicineList = list.compactMap { $0["Name"] as? String }
-                        print(self.medicineList)
-                    } else {
-                        print("Error: 'List' key not found in JSON response")
-                    }
-                } else {
-                    print("Error: Unable to parse JSON response")
-                }
-            } catch {
-                print("JSON Parsing Error: \(error)")
-            }
-        }
+                   defer { isLoading = false }
+                   if let error = error {
+                       print("Error: \(error.localizedDescription)")
+                       return
+                   }
+                   
+                   guard let data = data else {
+                       print("Error: No data received")
+                       return
+                   }
+                   
+                   do {
+                       if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                          let list = jsonResponse["List"] as? [[String: Any]] {
+                           self.medicineList = list.compactMap { dict in
+                               guard let name = dict["Name"] as? String, let code = dict["Code"] as? String else { return nil }
+                               return SearchMedicineModel(Name: name, Code: code)
+                           }
+                           print(self.medicineList)
+                       } else {
+                           print("Error: 'List' key not found in JSON response or unable to parse JSON response")
+                       }
+                   } catch {
+                       print("JSON Parsing Error: \(error)")
+                   }
+               }
         
         task.resume()
     }
