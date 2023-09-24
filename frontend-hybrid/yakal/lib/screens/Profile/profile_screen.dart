d2ee645142d384f9f71bfaf36366fac3e10c7e37
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-09-24 13:00:29 +0900

    feat : login setting

--- frontend-hybrid/yakal/lib/screens/Profile/profile_screen.dart
+++ frontend-hybrid/yakal/lib/screens/Profile/profile_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:yakal/screens/Auth/auth_login_screen.dart'; // Import the AuthLoginScreen
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'profile',
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Profile'),
+          const SizedBox(height: 20), // Add some spacing
+          ElevatedButton(
+            onPressed: () {
+              // 만약 뒤로 못돌아가게 하고 싶으면 Navigator.pushReplacement 사용
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const AuthLoginScreen()),
+              );
+            },
+            child: const Text('Login'),
+          ),
+        ],
       ),
     );
   }
