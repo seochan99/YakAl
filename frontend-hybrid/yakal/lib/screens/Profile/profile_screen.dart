610c729d28d63d4d9a59c31ec431df55b3dec2ea
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-09-25 15:42:34 +0900

    feat: profile header widget

--- frontend-hybrid/yakal/lib/screens/Profile/profile_screen.dart
+++ frontend-hybrid/yakal/lib/screens/Profile/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:yakal/viewModels/Profile/user_view_model.dart'; // Import the AuthLoginScreen
+import 'package:yakal/viewModels/Profile/user_view_model.dart';
+import 'package:yakal/widgets/Profile/profie_header_widget.dart'; // Import the AuthLoginScreen
 
 // 임시 유저 클래스
 
@@ -27,35 +28,7 @@ class ProfileScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Text(
-                          "${userViewModel.user.value.nickName}님,\n약알과 함께 건강하세요!"),
-                      const Spacer(),
-                      // 닉네임 수정 네모 버튼 안에는 수정 이라고 글자 적혀있다.
-                      GestureDetector(
-                        onTap: () {
-                          // 닉네임 수정 모달 띄우기
-                          // showEditNicknameModal = true;
-                        },
-                        child: Container(
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                              color: Colors.grey,
-                              width: 1,
-                            ),
-                          ),
-                          child: const Center(
-                            child: Text('수정'),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  ProfileHeader(userViewModel: userViewModel),
                   const SizedBox(height: 20),
                   GestureDetector(
                     onTap: () {
