256decf48bf81a0d761638664024c0f5772d0638
Nachanjin <skxkswls@gmail.com>
2023-09-26 23:43:56 +0900

    refactor : 유저 API 수정 및 Dto Valid 점검

--- backend-server/src/main/java/com/viewpharm/yakal/dto/response/MedicalRegisterDto.java
+++ backend-server/src/main/java/com/viewpharm/yakal/dto/response/MedicalRegisterDto.java
@@ -2,6 +2,9 @@ package com.viewpharm.yakal.dto.response;
 
 import com.viewpharm.yakal.type.EMedical;
 import com.viewpharm.yakal.type.ERecive;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,16 +14,25 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MedicalRegisterDto {
+    @Enumerated(EnumType.STRING)
     private EMedical medicalType;
+    @NotNull
     private String directorName;
+    @NotNull
     private String directorTel;
+    @NotNull
     private String medicalName;
+    @NotNull
     private String medicalTel;
+    @NotNull
     private String zipCode;
+    @NotNull
     private String medicalAddress;
     private String medicalDetailAddress;
+    @NotNull
     private String businessRegistrationNumber;
     private String imagePath;
+    @Enumerated(EnumType.STRING)
     private ERecive reciveType;
     private String medicalRuntime;
     private String medicalCharacteristics;
