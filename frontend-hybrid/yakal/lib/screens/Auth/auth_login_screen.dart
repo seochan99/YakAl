508d4fdc47f57b3088412ae5ce9e930ae63962b0
Dohun_Kim <raaak8128@gmail.com>
2023-09-27 13:58:04 +0900

    feat: Login Entry Page UI

--- frontend-hybrid/yakal/lib/screens/Auth/auth_login_screen.dart
+++ frontend-hybrid/yakal/lib/screens/Auth/auth_login_screen.dart
@@ -1,30 +1,110 @@
+import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
+import 'package:yakal/global/color_styles.dart';
+import 'package:yakal/widgets/Auth/social_login_button.dart';
 
 class AuthLoginScreen extends StatelessWidget {
+  bool get isiOS =>
+      foundation.defaultTargetPlatform == foundation.TargetPlatform.iOS;
+
   const AuthLoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.all(30.0),
-        child: Row(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorStyles.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "AI를 이용한 ",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "AI를 이용한 ",
+                        style: TextStyle(
+                          color: ColorStyles.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          height: 1.685,
+                          fontFamily: "SUIT",
+                        ),
+                      ),
+                      Text(
+                        "복약 도움 플랫폼",
+                        style: TextStyle(
+                          color: ColorStyles.main,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          height: 1.685,
+                          fontFamily: "SUIT",
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "약 알",
+                        style: TextStyle(
+                          color: ColorStyles.main,
+                          fontSize: 40,
+                          fontWeight: FontWeight.w700,
+                          height: 1.6,
+                          fontFamily: "SUIT",
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            Text(
-              "복약 도움 플랫폼",
-              style: TextStyle(
-                color: Color(0xFF2669f5),
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                "assets/images/login_banner.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                children: [
+                  SocialLoginButton(
+                    text: "카카오로 로그인",
+                    color: ColorStyles.black,
+                    backgroundColor: const Color(0xFFFEE500),
+                    iconPath: "assets/icons/kakao.svg",
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  isiOS
+                      ? SocialLoginButton(
+                          text: "Apple로 로그인",
+                          color: ColorStyles.white,
+                          backgroundColor: ColorStyles.black,
+                          iconPath: "assets/icons/apple.svg",
+                          onPressed: () {},
+                        )
+                      : SocialLoginButton(
+                          text: "Google 계정으로 로그인",
+                          color: ColorStyles.black,
+                          backgroundColor: ColorStyles.white,
+                          iconPath: "assets/icons/google.svg",
+                          onPressed: () {},
+                        ),
+                ],
               ),
             ),
           ],
