857a55263f1678a876c7f1f94d592e3a806a3fa8
Dohun_Kim <raaak8128@gmail.com>
2023-10-08 23:06:14 +0900

    fix: remove unnessesory video player

--- frontend-hybrid/yakal/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend-hybrid/yakal/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import flutter_secure_storage_macos
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
@@ -18,5 +17,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
