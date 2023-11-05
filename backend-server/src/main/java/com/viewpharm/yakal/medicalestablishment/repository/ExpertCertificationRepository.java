07022dbc1b2d0c50defddf9c99dae957ceb89f09
Kang Woobin <woobin929@dgu.ac.kr>
2023-11-06 02:37:02 +0900

    fix : 쿼리 수정

--- backend-server/src/main/java/com/viewpharm/yakal/medicalestablishment/repository/ExpertCertificationRepository.java
+++ backend-server/src/main/java/com/viewpharm/yakal/medicalestablishment/repository/ExpertCertificationRepository.java
@@ -22,22 +22,22 @@ public interface ExpertCertificationRepository extends JpaRepository<ExpertCerti
     @Query(value = "SELECT distinctrow u.id as ID, u.name as NAME, u.job as JOB, me.name as MEDICALNAME, u.tel as TEL, ec.created_at as DATE " +
             "From expert_certifications ec " +
             "inner join users u on u.id = ec.user_id " +
-            "inner join medical_establishments me on me.id = u.medical_establishment_id",
+            "inner join medical_establishments me on me.id = ec.medical_establishment_id",
             countQuery = "SELECT COUNT(distinctrow u.id, u.name, u.job, me.name, u.tel, ec.created_at) " +
                     "From expert_certifications ec " +
                     "inner join users u on u.id = ec.user_id " +
-                    "inner join medical_establishments me on me.id = u.medical_establishment_id", nativeQuery = true)
+                    "inner join medical_establishments me on me.id = ec.medical_establishment_id", nativeQuery = true)
     Page<ExpertCertificationInfo> findExpertCertificationInfo(Pageable pageable);
 
     @Query(value = "SELECT distinctrow u.id as ID, u.name as NAME, u.job as JOB, me.name as MEDICALNAME, u.tel as TEL, ec.created_at as DATE " +
             "From expert_certifications ec " +
             "inner join users u on u.id = ec.user_id " +
-            "inner join medical_establishments me on me.id = u.medical_establishment_id " +
-            "where u.name like '%:name%'",
+            "inner join medical_establishments me on me.id = ec.medical_establishment_id " +
+            "where u.name like %:name%",
             countQuery = "SELECT COUNT(distinctrow u.id, u.name, u.job, me.name, u.tel, ec.created_at) " +
                     "From expert_certifications ec " +
                     "inner join users u on u.id = ec.user_id " +
-                    "inner join medical_establishments me on me.id = u.medical_establishment_id " +
+                    "inner join medical_establishments me on me.id = ec.medical_establishment_id " +
                     "where u.name like %:name%", nativeQuery = true)
     Page<ExpertCertificationInfo> findExpertCertificationInfoByName(@Param("name") String name, Pageable pageable);
 
