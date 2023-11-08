655c74e6442ce1c3b33ee8d8f31d60f884edff99
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-11-08 11:30:29 +0900

    fix: 전문가 신청 오류 해결

--- backend-server/src/main/java/com/viewpharm/yakal/base/type/EJob.java
+++ backend-server/src/main/java/com/viewpharm/yakal/base/type/EJob.java
@@ -3,5 +3,20 @@ package com.viewpharm.yakal.base.type;
 public enum EJob {
     PATIENT,
     DOCTOR,
-    PHARMACIST,
+    PHARMACIST;
+
+    // EJob to ERole
+    public ERole toEole() {
+        switch (this) {
+            case PATIENT -> {
+                return ERole.USER;
+            }
+            case DOCTOR -> {
+                return ERole.DOCTOR;
+            }
+            case PHARMACIST -> {
+                return ERole.PHARMACIST;
+            }
+        }
+    }
 }
