dc5b1b0413dd82b803780649ac7b717c2de529c6
Nachanjin <skxkswls@gmail.com>
2023-11-16 07:27:37 +0900

    fix prescritionid remove

--- backend-server/src/main/java/com/viewpharm/yakal/prescription/repository/PrescriptionRepository.java
+++ backend-server/src/main/java/com/viewpharm/yakal/prescription/repository/PrescriptionRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
 
     List<Prescription> findByUserId(Long userId);
+    Optional<Prescription> findTop1ByUserId(Long userId);
 
 }
